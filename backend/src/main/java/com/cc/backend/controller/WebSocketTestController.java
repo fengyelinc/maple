63928deafcbde044fc79ec0c80a7a8c14a9c17ee
chenchen <fengyelinc@163.com>
2024-02-28 11:07:47 +0800

    add：websocket配置

---
+++ backend/src/main/java/com/cc/backend/controller/WebSocketTestController.java
@@ -0,0 +1,19 @@
+package com.cc.backend.controller;
+
+import com.cc.backend.websocket.WebSocketServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class WebSocketTestController {
+
+    @RequestMapping("/push/{toUserId}")
+    public ResponseEntity<String> pushToWeb(String message, @PathVariable String toUserId) throws IOException {
+        WebSocketServer.sendInfo(message,toUserId);
+        return ResponseEntity.ok("MSG SEND SUCCESS");
+    }
+}
